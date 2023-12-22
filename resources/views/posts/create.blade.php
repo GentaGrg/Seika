814101bea0bc6c04282be56066f2250b2cd74bdb
GentaGrg <gentagoolung@gmail.com>
2023-12-22 15:48:21 +0900

    未完成

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,23 +2,46 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Posts</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <title>Blog</title>
     </head>
     <body>
-        <h1 class="title">
-            {{ $post->title }}
-        </h1>
-        <div class="content">
-            <div class="content__post">
-                <h3>本文</h3>
-                <p>{{ $post->body }}</p>    
+        <h1>Blog Name</h1>
+        <form action="/posts" method="POST">
+            @csrf
+            <div class="title">
+                <h2>Title</h2>
+                <input type="text" name="post[title]" placeholder="タイトル" value="{{ old('post.title') }}"/>
+                <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
             </div>
-        </div>
-        <div class="footer">
-            <a href="/">戻る</a>
-        </div>
+            <div class="body">
+                <h2>Body</h2>
+                <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。">{{ old('post.body') }}</textarea>
+                <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
+            </div>
+            <input type="submit" value="保存"/>
+        </form>
+        <div class="back">[<a href="/">back</a>]</div>
     </body>
+@extends('layouts.app')
+
+@section('content')
+    <h2>Create a New Post</h2>
+
+    <form method="post" action="{{ route('posts.store') }}">
+        @csrf
+        <div>
+            <label for="title">Title:</label>
+            <input type="text" name="title" id="title" value="{{ old('title') }}" required>
+        </div>
+
+        <div>
+            <label for="content">Content:</label>
+            <textarea name="content" id="content" required>{{ old('content') }}</textarea>
+        </div>
+
+        <div>
+            <button type="submit">Create Post</button>
+        </div>
+    </form>
+@endsection
 </html>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,16 +3,25 @@
     <head>
         <meta charset="utf-8">
         <title>Blog</title>
+        
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    
     </head>
     <body>
-        <h1>Blog Name</h1>
-        <div class='posts'>
-            <div class='post'>
-                <h2 class='title'>Title</h2>
-                <p class='body'>This is a sample body.</p>
-            </div>
+    @extends('layouts.app')
+
+@section('content')
+    <h2>Posts</h2>
+
+    <a href="{{ route('posts.create') }}">Create a New Post</a>
+
+    @foreach ($posts as $post)
+        <div>
+            <h3>{{ $post->title }}</h3>
+            <p>{{ $post->content }}</p>
         </div>
+    @endforeach
+@endsection
     </body>
 </html>
