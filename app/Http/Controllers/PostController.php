de7073ec71b315c20ea4def930321dce0e3835a8
GentaGrg <gentagoolung@gmail.com>
2023-12-24 18:09:51 +0900

    tuika

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,29 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 use App\Models\Post;
 
 class PostController extends Controller
 {
     public function index(Post $post)
-{
-    return view('posts.index')->with(['posts' => $post->getPaginateByLimit(1)]);
+    {
+    return view('posts.index')->with(['posts' => $post->paginate(1)]);
     }
-
+    
+    public function show(Post $post)
+    {
+        return view('posts/show')->with(['post' => $post]);
+    }
+    
     public function create()
     {
-        return view('posts.create');
+        return view('posts/create');
     }
-
-    public function store(Request $request)
+    
+    public function store(PostRequest $request, Post $post)
     {
-        $request->validate([
-            'title' => 'required|max:255',
-            'content' => 'required',
-        ]);
-
-        Post::create($request->all());
-
-        return redirect()->route('posts.index');
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    
+    public function edit(Post $post)
+    {
+        return view('posts/edit')->with(['post' => $post]);
+    }
+    
+    public function update(PostRequest $request, Post $post)
+    {
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+        return redirect('/posts/' . $post->id);
     }
 }
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -2,26 +2,31 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Posts</title>
+        <title>Blog</title>
+        
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    
     </head>
-    <body>
-    <h1 class="title">編集画面</h1>
-    <div class="content">
+    <body class="antialiased">
+        <h1>Blog Name</h1>
         <form action="/posts/{{ $post->id }}" method="POST">
             @csrf
             @method('PUT')
-            <div class='content__title'>
-                <h2>タイトル</h2>
-                <input type='text' name='post[title]' value="{{ $post->title }}">
+            <div class="title">
+                <h2>Title</h2>
+                <input type="text" name=post[title] placeholder="タイトル" value={{ $post->title }}>
+                <p class='title__error' style="color:red">{{ $errors->first('post.title') }} </p>
             </div>
-            <div class='content__body'>
-                <h2>本文</h2>
-                <input type='text' name='post[body]' value="{{ $post->body }}">
+            <div class="body">
+                <h2>Body</h2>
+                <textarea name="post[body]" placeholder="今日も一日お疲れ様でした。">{{ $post->body }}</textarea>
+                <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
             </div>
-            <input type="submit" value="保存">
+               <input type="submit" value="update">
         </form>
-    </div>
-</body>
+        <div class='footer'>
+            <a href="/posts/{{ $post->id }}">戻る</a>
+        </div>
+    </body>
+</html>
