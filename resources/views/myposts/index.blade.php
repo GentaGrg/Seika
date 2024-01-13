78f8a4af0f12ddc781faab95d4efd6b370e88be0
GentaGrg <gentagoolung@gmail.com>
2024-01-13 16:32:02 +0900

    1-13mikan

--- resources/views/myposts/index.blade.php
+++ resources/views/myposts/index.blade.php
@@ -1,11 +1,10 @@
-<!-- resources/views/myposts/index.blade.php -->
 <x-app-layout>
     <x-slot name="header">
     </x-slot>
 
     <h1>自分の投稿一覧</h1>
 
-    @if($userPosts)
+    @if($userPosts && count($userPosts) > 0)
         @foreach($userPosts as $post)
             <h2>{{ $post->title }}</h2>
             <p>{{ $post->body }}</p>
